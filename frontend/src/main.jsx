c41c50fbf5393b3e7a1aea1796c21e285b612ecd
Tanya Yadav <ty6710302@gmail.com>
2026-09-02 22:47:41 +0530

    Load final mobile responsive fixes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import './responsive.css'
+import './mobile-fixes.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
