2c3082af8fd973aa1e83ce0b9533fa3dcc28080d
Tanya Yadav <ty6710302@gmail.com>
2026-09-03 00:44:25 +0530

    Remove problematic mobile override import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,6 @@ import App from './App.jsx'
 import './responsive.css'
 import './mobile-fixes.css'
 import './mobile-final.css'
-import './mobile-final-override.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
